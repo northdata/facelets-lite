eac48cced37d15e8c3163725ab6d64fd5fd5f28d
Frank Felix Debatin <reezom@users.noreply.github.com>
2020-10-01 16:18:21 +0200

    Update EscapeAwareHtmlWriter.java

--- src/main/java/org/faceletslite/imp/EscapeAwareHtmlWriter.java
+++ src/main/java/org/faceletslite/imp/EscapeAwareHtmlWriter.java
@@ -19,40 +19,39 @@ import org.dom4j.io.OutputFormat;
  */
 public class EscapeAwareHtmlWriter extends HTMLWriter {
 
-	public EscapeAwareHtmlWriter(Writer writer, OutputFormat format) {
-		super(writer, format);
-	}
+    public EscapeAwareHtmlWriter(Writer writer, OutputFormat format) {
+        super(writer, format);
+    }
 
-	@Override
-	protected void writeProcessingInstruction(ProcessingInstruction pi) throws IOException {
-		if (pi.getTarget().equals(StreamResult.PI_DISABLE_OUTPUT_ESCAPING)) {
-			setEscapeText(false);
-		} else if (pi.getTarget().equals(StreamResult.PI_ENABLE_OUTPUT_ESCAPING)) {
-			setEscapeText(true);
-		}
-	}
+    @Override
+    protected void writeProcessingInstruction(ProcessingInstruction pi) throws IOException {
+        if (pi.getTarget().equals(StreamResult.PI_DISABLE_OUTPUT_ESCAPING)) {
+            setEscapeText(false);
+        } else if (pi.getTarget().equals(StreamResult.PI_ENABLE_OUTPUT_ESCAPING)) {
+            setEscapeText(true);
+        }
+    }
 
-	/**
-	 * Javascript sections need unescaped text in CDATA.
-	 */
-	@Override
-	public void writeElementContent(Element element) throws IOException {
-		if ("script".equalsIgnoreCase(element.getName())) {
-			String type = element.attributeValue("type");
-			if (type != null && type.contains("/javascript")) {
-				setEscapeText(false);
-				println();
-				writer.write(" // <![CDATA[");
-				println();
-				super.writeElementContent(element);
-				println();
-				writer.write(" // ]]>");
-				println();
-				setEscapeText(true);
-			}
-		}
-		else {
-			super.writeElementContent(element);
-		}
-	}
+    /**
+     * Javascript sections need unescaped text in CDATA.
+     */
+    @Override
+    public void writeElementContent(Element element) throws IOException {
+        if ("script".equalsIgnoreCase(element.getName())) {
+            String type = element.attributeValue("type");
+            if (type != null && type.contains("/javascript")) {
+                setEscapeText(false);
+                println();
+                writer.write(" // <![CDATA[");
+                println();
+                super.writeElementContent(element);
+                println();
+                writer.write(" // ]]>");
+                println();
+                setEscapeText(true);
+                return;
+            }
+        }
+        super.writeElementContent(element);
+    }
 }
