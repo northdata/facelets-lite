30c3dcd50f7241cf20a0f3edf9d2992c305f1b36
Harald Wellmann <harald.wellmann@northdata.de>
2021-04-01 11:19:02 +0200

    enable hot reloading in the ClasspathResourceReader

--- src/main/java/org/faceletslite/imp/ClasspathResourceReader.java
+++ src/main/java/org/faceletslite/imp/ClasspathResourceReader.java
@@ -1,8 +1,12 @@
 package org.faceletslite.imp;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.util.jar.JarFile;
+import java.util.zip.ZipEntry;
 
 import org.faceletslite.ResourceReader;
 import org.slf4j.Logger;
@@ -11,7 +15,7 @@ import org.slf4j.LoggerFactory;
 /**
  * Loads resources from the classpath.
  * <p>
- * TODO: move to facelets-lite.
+ * For development, hot reloading can be enabled.
  *
  * @author hwellmann
  *
@@ -22,13 +26,19 @@ public class ClasspathResourceReader implements ResourceReader {
 
     private final String rootDir;
     private final String defaultExtension;
+    private final boolean reload;
 
     public ClasspathResourceReader(String rootDir, String defaultExtension) {
+        this(rootDir, defaultExtension, false);
+    }
+
+    public ClasspathResourceReader(String rootDir, String defaultExtension, boolean reload) {
         this.rootDir = rootDir;
         if (!defaultExtension.startsWith(".")) {
             defaultExtension = "." + defaultExtension;
         }
         this.defaultExtension = defaultExtension;
+        this.reload = true;
     }
 
     public String getRootDir() {
@@ -53,7 +63,8 @@ public class ClasspathResourceReader implements ResourceReader {
         if (filename.startsWith("/")) {
             filename = filename.substring(1);
         }
-        InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream(filename);
+        InputStream is = getInputStream(filename);
+
         if (is == null) {
             String msg = "cannot find '" + filename + "'";
             log.error(msg);
@@ -61,4 +72,39 @@ public class ClasspathResourceReader implements ResourceReader {
         }
         return is;
     }
+
+    private InputStream getInputStream(String filename) throws IOException {
+        if (reload) {
+            return getInputStreamWithReload(filename);
+        } else {
+            return getInputStreamFromClassloader(filename);
+        }
+    }
+
+    private InputStream getInputStreamWithReload(String filename) throws IOException {
+        ClassLoader ccl = Thread.currentThread().getContextClassLoader();
+        URL resource = ccl.getResource(filename);
+        if (resource == null) {
+            return null;
+        }
+
+        String url = resource.toString();
+        if (!url.startsWith("jar:file:")) {
+            return getInputStreamFromClassloader(filename);
+        }
+
+        int excl = url.indexOf('!');
+        url = url.substring("jar:file:".length(), excl);
+
+        JarFile jarFile = new JarFile(new File(url));
+        ZipEntry entry = jarFile.getEntry(filename);
+        InputStream is = jarFile.getInputStream(entry);
+        return new JarEntryInputStream(jarFile, is);
+    }
+
+    private InputStream getInputStreamFromClassloader(String filename) throws IOException {
+        InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream(filename);
+        return is;
+    }
+
 }
---
+++ src/main/java/org/faceletslite/imp/JarEntryInputStream.java
@@ -0,0 +1,52 @@
+package org.faceletslite.imp;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.jar.JarFile;
+
+/**
+ * Wraps an input stream for a JAR file entry, closing
+ * the entire JAR file when the entry stream is closed.
+ * 
+ * @author hwellmann
+ *
+ */
+public final class JarEntryInputStream extends InputStream {
+
+    private final JarFile jarFile;
+    private final InputStream stream;
+
+    public JarEntryInputStream(JarFile jarFile, InputStream stream) {
+        this.jarFile = jarFile;
+        this.stream = stream;
+    }
+
+    public void close() throws IOException {
+        stream.close();
+        jarFile.close();
+    }
+
+    public void mark(int readlimit) {
+        stream.mark(readlimit);
+    }
+
+    public boolean markSupported() {
+        return stream.markSupported();
+    }
+
+    public int read() throws IOException {
+        return stream.read();
+    }
+
+    public int read(byte b[], int off, int len) throws IOException {
+        return stream.read(b, off, len);
+    }
+
+    public void reset() throws IOException {
+        stream.reset();
+    }
+
+    public long skip(long n) throws IOException {
+        return stream.skip(n);
+    }
+}
